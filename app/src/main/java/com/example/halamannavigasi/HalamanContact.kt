aba2ee9b0f5888ad3e6e67014b5660bacdbfbd3b
MuhFadila <muh.fadila18@gmail.com>
2023-11-15 18:50:11 +0700

    add 3 OutlinedTextField

--- app/src/main/java/com/example/halamannavigasi/HalamanContact.kt
+++ app/src/main/java/com/example/halamannavigasi/HalamanContact.kt
@@ -2,15 +2,22 @@ package com.example.halamannavigasi
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun HalamanContact(
     onConfirmButtonClicked: (String, String, String) -> Unit,
@@ -24,5 +31,34 @@ fun HalamanContact(
         modifier = Modifier
             .fillMaxSize()
             .padding(horizontal = 16.dp, vertical = 16.dp)
-    ) {}
+    ) {
+        OutlinedTextField(
+            value = nama,
+            onValueChange = { nama = it },
+            label = { Text(text = "Nama Pelanggan") },
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(bottom = 8.dp),
+        )
+
+        OutlinedTextField(
+            value = noHp,
+            onValueChange = { noHp = it },
+            label = { Text(text = "Nomor Telepon") },
+            keyboardOptions = KeyboardOptions.Default.copy(
+                keyboardType = KeyboardType.Phone
+            ),
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(bottom = 8.dp),
+        )
+        OutlinedTextField(
+            value = alamat,
+            onValueChange = { alamat = it },
+            label = { Text(text = "Alamat") },
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(bottom = 16.dp),
+        )
+    }
 }
