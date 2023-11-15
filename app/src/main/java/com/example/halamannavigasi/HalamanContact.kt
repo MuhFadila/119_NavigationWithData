e91f4dbe3934c997d91ee8c4d3fd6d265cd28e80
MuhFadila <muh.fadila18@gmail.com>
2023-11-15 18:47:16 +0700

    add var

--- app/src/main/java/com/example/halamannavigasi/HalamanContact.kt
+++ app/src/main/java/com/example/halamannavigasi/HalamanContact.kt
@@ -1,9 +1,17 @@
 package com.example.halamannavigasi
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 
 @Composable
 fun HalamanContact(
     onConfirmButtonClicked: (String, String, String) -> Unit,
     onCancelButtonClicked: () -> Unit
-){}
+){
+    var nama by remember { mutableStateOf("") }
+    var noHp by remember { mutableStateOf("") }
+    var alamat by remember { mutableStateOf("") }
+}
